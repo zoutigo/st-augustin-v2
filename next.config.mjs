3782e209b12ef6b43762c8bb87eba5343637bb5f
zoutigo <zoutigo@gmail.com>
2024-09-05 06:40:07 +0200

    change resolver definition

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve('./');
+    return config;
+  },
+};
 
 export default nextConfig;
