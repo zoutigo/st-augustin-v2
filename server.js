ced5a584208861d56658325d36d3c426b725768d
zoutigo <zoutigo@gmail.com>
2024-09-06 14:23:19 +0200

    include build and server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import { createServer } from 'http';
-import { parse } from 'url';
-import next from 'next';
+const { createServer } = require('http');
+const { parse } = require('url');
+const { next } = require('next');
 
 const port = parseInt(process.env.PORT || '3000', 10);
 const dev = process.env.NODE_ENV !== 'production';
@@ -9,7 +9,7 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true);
+    const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
   }).listen(port);
 
