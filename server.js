aee3a81dc873c4d9660a38e925c1da736d88af29
zoutigo <zoutigo@gmail.com>
2024-09-06 14:31:25 +0200

    update  server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { createServer } = require('http');
 const { parse } = require('url');
-const { next } = require('next');
+const next = require('next');
 
 const port = parseInt(process.env.PORT || '3000', 10);
 const dev = process.env.NODE_ENV !== 'production';
