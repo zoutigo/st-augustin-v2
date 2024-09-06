7e48cc5407e280b01a4bee2b31fd751ee8baadf5
zoutigo <zoutigo@gmail.com>
2024-09-06 14:42:00 +0200

    updated build for debug

--- server.js
+++ server.js
@@ -7,15 +7,23 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
-  }).listen(port);
+console.log('environment:', dev);
 
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  );
-});
+app
+  .prepare()
+  .then(() => {
+    createServer((req, res) => {
+      const parsedUrl = parse(req.url, true);
+      handle(req, res, parsedUrl);
+    }).listen(port, (err) => {
+      if (err) throw err;
+      console.log(
+        `> Server listening at http://localhost:${port} in ${
+          dev ? 'development' : 'production'
+        } mode`
+      );
+    });
+  })
+  .catch((err) => {
+    console.error('Error starting Next.js server:', err);
+  });
