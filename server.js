2964e8ad10d066b7cb7de3046b06c1c90c5e5666
zoutigo <zoutigo@gmail.com>
2024-09-06 15:01:59 +0200

    show all env variables

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+console.log('All environment variables:', process.env);
+
 console.log('environment:', dev);
 const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET;
 
