c9cbbe6e0bb02039aae396f2ccf0958dba9b8979
zoutigo <zoutigo@gmail.com>
2024-09-14 05:46:58 +0200

    logo installed

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -5,6 +5,7 @@ import { cn } from '@/lib/utils';
 import { Poppins } from 'next/font/google';
 import { NavButton } from './navButton';
 import { NavRoutes } from '@/routes';
+import { Logo } from './logo';
 
 const font = Poppins({
   subsets: ['latin'],
@@ -15,8 +16,11 @@ export const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <header className="z-10 flex flex-row justify-between items-center h-[7vh] min-w-[100%] px-10 shadow-sm">
-      <div className="bg-yellow-100 w-[20%]"> Logo</div>
+    <header className="z-10 flex flex-row justify-between items-center h-[14vh] min-w-[100%] px-10 shadow-sm">
+      <div className="mt-7">
+        <Logo />
+      </div>
+      {/* <div className="bg-yellow-100 w-[20%]"> Logo</div> */}
       <nav className=" bg-white w-[80%] flex flew-row justify-between items-center m-x-2">
         {NavRoutes.map((route) => {
           return (
