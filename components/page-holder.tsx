2198c1d50dd28e4e880019ec271360f124c95e61
zoutigo <zoutigo@gmail.com>
2025-01-10 15:09:06 +0100

    pages and entitypages

--- components/page-holder.tsx
+++ components/page-holder.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import Breadcrumb from './breadcrumb';
+
 interface PageHolderProps {
   children: React.ReactNode;
 }
 
-export const PageHolder = ({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) => {
+export const PageHolder: React.FC<PageHolderProps> = ({ children }) => {
   return (
     <div className="mx-[12%]">
       <Breadcrumb />
-      {children}
+      <div className="min-w-full">{children}</div>
     </div>
   );
 };
