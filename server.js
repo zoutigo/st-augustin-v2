5c1692fd4e9b030dc53b317f4e1a67ca4754e4a5
zoutigo <zoutigo@gmail.com>
2025-01-15 01:50:02 +0100

    few changes on server

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET;
+const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 if (!NEXTAUTH_SECRET) {
   throw new Error('NEXTAUTH_SECRET environment variable is not defined');
@@ -28,11 +29,14 @@ app
     }).listen(port, (err) => {
       if (err) throw err;
       console.log(
-        `> Server listening at http://localhost:${port} in ${
-          dev ? 'development' : 'production'
-        } mode`
+        `> Server listening at ${
+          dev ? `http://localhost:${port}` : NEXT_PUBLIC_BASE_URL
+        }`
       );
     });
+    //   `> Server listening atNEXT_PUBLIC_BASE_URL in ${
+    //     dev ? 'development' : NEXT_PUBLIC_BASE_URL
+    //   } mode`
   })
   .catch((err) => {
     console.error('Error starting Next.js server:', err);
