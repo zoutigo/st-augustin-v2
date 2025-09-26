f53a1d8496111c57ea68654646870c631c8dbfb5
zoutigo <zoutigo@gmail.com>
2025-09-26 07:21:16 +0200

    fix button position

--- components/landing/favorites/landing-favorite-card.tsx
+++ components/landing/favorites/landing-favorite-card.tsx
@@ -21,19 +21,19 @@ export const LandingFavoriteCard = ({
   path,
 }: LandingFavoriteCardProps) => {
   return (
-    <Card>
+    <Card className="h-full flex flex-col">
       <CardHeader>
         <div className="flex justify-center">
           <Icon className="text-4xl text-primary h-[6rem] w-[6rem]" />{' '}
         </div>
       </CardHeader>
-      <CardContent>
+      <CardContent className="flex-1">
         <div className="text-center text-secondary font-sans font-semibold text-2xl my-[2rem] uppercase">
           {title}{' '}
         </div>
         <div className="text-xl text-justify">{text} </div>
       </CardContent>
-      <CardFooter className="mt-4">
+      <CardFooter className="mt-auto">
         <Button
           variant={'default'}
           className="w-full py-8 hover:bg-secondary-ligth hover:uppercase"
