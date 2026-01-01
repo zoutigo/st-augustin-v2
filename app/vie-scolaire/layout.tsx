e9c62d51e391ce6c3879069dd21a7ba1edc25fae
zoutigo <zoutigo@gmail.com>
2026-01-01 18:35:35 +0100

    fix ssr

--- app/vie-scolaire/layout.tsx
+++ app/vie-scolaire/layout.tsx
@@ -1,5 +1,4 @@
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
+export const revalidate = 3600;
 
 const VieScolaireLayout = ({ children }: { children: React.ReactNode }) => {
   return <>{children}</>;
