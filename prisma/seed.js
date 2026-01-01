e02c10f088ee1bff86885a8faf40ef91b7a98a3f
zoutigo <zoutigo@gmail.com>
2026-01-01 02:53:53 +0100

    fix: seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,11 +1,27 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 require("dotenv").config();
+const { PrismaClient } = require("@prisma/client");
 const { ensureDefaultPages } = require("../lib/ensure-default-pages");
 const { ensureDefaultEntities } = require("../lib/ensure-default-entities");
 
+const prisma = new PrismaClient();
+
 async function main() {
-  await ensureDefaultPages();
-  await ensureDefaultEntities({ postsPerEntity: 5 });
+  const pageCount = await prisma.page.count();
+  if (pageCount > 0) {
+    // eslint-disable-next-line no-console
+    console.log("[seed] Pages déjà présentes, skip ensureDefaultPages()");
+  } else {
+    await ensureDefaultPages();
+  }
+
+  const entityCount = await prisma.entity.count();
+  if (entityCount > 0) {
+    // eslint-disable-next-line no-console
+    console.log("[seed] Entités déjà présentes, skip ensureDefaultEntities()");
+  } else {
+    await ensureDefaultEntities({ postsPerEntity: 5 });
+  }
 }
 
 main()
@@ -17,4 +33,7 @@ main()
     // eslint-disable-next-line no-console
     console.error("Seed erreur", e);
     process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
   });
